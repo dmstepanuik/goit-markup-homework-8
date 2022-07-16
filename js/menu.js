9824c74f1719f7c89a6ce6ce11556794c83805c0
dmstepanuik <0669218646@ukr.net>
2022-07-16 08:49:27 +0300

    add mob menu

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('.menu-open-btn'),
-    closeMenuBtn: document.querySelector('.close-open-btn'),
-    menu: document.querySelector('.mob-menu'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
+    menu: document.querySelector('.mobile-menu'),
     body: document.querySelector('body'),
   };
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
   function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
+    refs.menu.classList.toggle('is-open');
     refs.body.classList.toggle('no-scroll');
   }
 })();
